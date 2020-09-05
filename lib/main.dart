ce169d5368cfd387b22adc09595fb5ddf0e17d9b
Courtney Calagaz <cnc0050@auburn.edu>
2020-09-05 15:25:55 -0500

    Sign In

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,16 @@ import 'package:crypto/crypto.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
+
+//import 'package:flutter_signin_button/register_page.dart';
+//import 'package:flutter_signin_button/signin_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -117,6 +127,62 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AuthApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Firebase Example App',
+        theme: ThemeData.dark(),
+        home: Scaffold(
+          body: AuthTypeSelector(),
+        ));
+  }
+}
+
+/// Provides a UI to select a authentication type page
+class AuthTypeSelector extends StatelessWidget {
+  // Navigates to a new page
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (_) => page),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase Example App"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: SignInButtonBuilder(
+              icon: Icons.person_add,
+              backgroundColor: Colors.indigo,
+              text: 'Registration',
+              // onPressed: () => _pushPage(context, RegisterPage()),
+            ),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+          Container(
+            child: SignInButtonBuilder(
+              icon: Icons.verified_user,
+              backgroundColor: Colors.orange,
+              text: 'Sign In',
+              // onPressed: () => _pushPage(context, SignInPage()),
+            ),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+        ],
+      ),
     );
   }
 }
